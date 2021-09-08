0416c91c3a795134c9b254629e552bb1bfaf70a7
hallotype <thomjanssen128@gmail.com>
2021-09-08 10:34:06 +0200

    My First Commit

---
+++ src/main/java/nl/thom/MyFirstClass.java
@@ -0,0 +1,11 @@
+package nl.thom;
+
+public class MyFirstClass {
+
+    // Fully qualitied name:
+    // nl.thom.MyFirstClass
+
+    public static void main(String[] args) { // main methode
+        System.out.println("Hello, World!");
+    }
+}
